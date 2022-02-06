0c5e611a015cf3f0432576922320c3605747adcf
lvlahraam <lvlahraam@gmail.com>
2022-02-06 19:58:07 +0330

    Spawn

--- bot.py
+++ bot.py
@@ -3,7 +3,11 @@ from core.views import confirm, pagination
 import core.utils.help as help
 from discord.ext import commands
 
-logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("discord")
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
+logger.addHandler(handler)
 
 async def create_pool_postgres():
     bot.postgres = await asyncpg.create_pool(dsn=os.getenv("DATABASE_URL"))
