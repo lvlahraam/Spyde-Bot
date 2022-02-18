ce65c21c68cd863aaa247613fa35daed647c3af0
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2022-02-18 15:22:11 +0330

    Spawn

--- core/commands/economy.py
+++ core/commands/economy.py
@@ -5,7 +5,7 @@ class Economy(commands.Cog, description="Buying Bitcoin and NFTs with these!"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="addmoney", description="Adds the given amount of momey to you or the given user")
+    @commands.command(name="addmoney", aliases=["am"], help="Adds the given amount of momey to you or the given user")
     @commands.is_owner()
     async def addmoney(self, ctx:commands.Context, amount:int=commands.Option(description="The amount you want give"), user:discord.User=commands.Option(description="The user you want to give the amount to", default=None)):
         user = user or ctx.author
