98b74c1e5c407ae0403a3c30d86779c9a2996847
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2022-02-25 03:07:42 +0330

    Update connection.py

--- core/events/connection.py
+++ core/events/connection.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from core.views import ticket
+
 class OnConnect(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -21,7 +22,7 @@ class OnReady(commands.Cog):
             self.bot.add_view(ticket.TicketView(self.bot))
             self.bot.add_view(ticket.CloseTicketView(self.bot, None, None))
             self.bot.persistent_views_added = True
-        await self.bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name=F"over Sym {len(self.bot.guilds)} | {self.bot.default_prefix}help"))
+        await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=F"over {len(self.bot.guilds)} G's & {len(self.bot.users)} U's"))
 
 class OnDisconnect(commands.Cog):
     def __init__(self, bot):
