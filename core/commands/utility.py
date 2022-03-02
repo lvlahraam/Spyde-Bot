3d8d1eaebb11b9c748e1f480906fa811cd07da6b
lvlahraam <lvlahraam@gmail.com>
2022-03-02 09:56:15 +0330

    Update utility.py

--- core/commands/utility.py
+++ core/commands/utility.py
@@ -49,43 +49,55 @@ class Utility(commands.Cog, description="Useful stuff that are open to everyone"
     @commands.guild_only()
     @commands.has_permissions(administrator=True)
     @commands.bot_has_permissions(administrator=True)
-    async def backup(self, ctx:commands.Context, option:typing.Literal["create", "delete", "list"]=commands.Option(description="The option you want to use"), *, value:str=commands.Option(description="The value you want to use", default=None)):
+    async def backup(self, ctx:commands.Context, option:typing.Literal["create", "delete", "info", "list", "load"]=commands.Option(description="The option you want to use"), *, value:str=commands.Option(description="The value you want to use", default=None)):
         bumbed = discord.Embed(
             color=self.bot.color,
             timestamp=ctx.message.created_at
         )
         bumbed.set_footer(text=ctx.author, icon_url=ctx.author.display_avatar.url)
         if option == "create":
-            view = confirm.ViewConfirm(ctx)
-            view.message = await ctx.reply(content="Are you sure if you want to create a backup for this server?", view=view)
-            await view.wait()
-            if view.value:
-                bumbed.title = "Creating backup..."
-                message = await ctx.reply(embed=bumbed)
-                channels = []
-                for channel in ctx.guild.channels:
-                    channels.append(channel)
-                roles = []
-                for role in ctx.guild.roles:
-                    roles.append(role)
-                emojis = []
-                for emoji in ctx.guild.emojis:
-                    emojis.append(emoji)
-                stickers = []
-                for sticker in ctx.guild.stickers:
-                    stickers.append(sticker)
-                name = "".join(random.choice(F"{string.ascii_uppercase}{string.digits}") for _ in range(10))
-                await self.bot.mongodb.backups.insert_one({"name": name, "user_name": ctx.author, "user_id": ctx.author.id, "guild_name": ctx.guild, "guild_id": ctx.guild.id, "channels": channels, "roles": roles, "emojis": emojis, "stickers": stickers, "icon": ctx.guild.icon, "banner": ctx.guild.banner, "time": discord.utils.utcnow()})
-                bumbed.title = "Backup has been created..."
-                bumbed.description = F"Under the name of **{name}**"
+            backups = await self.bot.mongodb.backups.find({"user_id": ctx.author.id}).to_list(length=25)
+            if len(backups) > 25:
+                view = confirm.ViewConfirm(ctx)
+                view.message = await ctx.reply(content="Are you sure if you want to create a backup for this server?", view=view)
+                await view.wait()
+                if view.value:
+                    bumbed.title = "Creating backup..."
+                    message = await ctx.reply(embed=bumbed)
+                    channels = []
+                    for channel in ctx.guild.channels:
+                        channels.append(channel)
+                    roles = []
+                    for role in ctx.guild.roles:
+                        roles.append(role)
+                    emojis = []
+                    for emoji in ctx.guild.emojis:
+                        emojis.append(emoji)
+                    stickers = []
+                    for sticker in ctx.guild.stickers:
+                        stickers.append(sticker)
+                    name = "".join(random.choice(F"{string.ascii_uppercase}{string.digits}") for _ in range(10))
+                    await self.bot.mongodb.backups.insert_one({"name": name, "user_name": ctx.author.name, "user_id": ctx.author.id, "guild_name": ctx.guild.name, "guild_id": ctx.guild.id, "channels": channels, "roles": roles, "emojis": emojis, "stickers": stickers, "icon": ctx.guild.icon.url if ctx.guild.icon else None, "banner": ctx.guild.banner.url if ctx.guild.banner else None, "time": discord.utils.utcnow()})
+                    bumbed.title = "Backup has been created..."
+                    bumbed.description = F"Under the name of **{name}**"
+            bumbed.title = "You can't create more than 25 backups"
         elif option == "delete":
             if value:
                 backup = await self.bot.mongodb.backups.find_one({"name": value, "user_id": ctx.author.id})
                 if backup:
                     bumbed.title = F"Backup Info - {backup['guild_name']} / [{backup['name']}]"
-                    bumbed.description = F"{discord.utils.format_dt(backup['time'], style='F')} ({discord.utils.format_dt(backup['time'], style='R')})"
-                    bumbed.add_field(name="Channels:", value="\n".join(f'{channel.name}' for channel in backup['channels']))
-                    bumbed.add_field(name="Roles:", value="\n".join(f'{role.name}' for role in backup['roles']))
+                    chpag = commands.Paginator(suffix="...\n```", max_size=2048)
+                    for channel in backup['channels']:
+                        if type(channel) == discord.CategoryChannel: known = "/"
+                        if type(channel) == discord.TextChannel: known = "#"
+                        if type(channel) == discord.VoiceClient: known = ">"
+                        if type(channel) == discord.StageChannel: known = ")"
+                        chpag.add_line(F"{known} {channel.name}")
+                    ropag = commands.Paginator(suffix="...\n```", max_size=2048)
+                    for role in backup['roles']:
+                        ropag.add_line(role.name)
+                    if backup['icon']: bumbed.set_image(url=backup['icon'])
+                    if backup['banner']: bumbed.set_thumbnail(url=backup['banner'])
                     view = confirm.ViewConfirm(ctx)
                     view.message = await ctx.reply(content="Are you sure if you want to delete this backup?", embed=bumbed, view=view)
                     await view.wait()
@@ -96,6 +108,81 @@ class Utility(commands.Cog, description="Useful stuff that are open to everyone"
                     bumbed = "Couldn't find any backup with this name"
             else:
                 bumbed = "You must pass an name"
+        elif option == "info":
+            if value:
+                backup = await self.bot.mongodb.backups.find_one({"name": value, "user_id": ctx.author.id})
+                if backup:
+                    bumbed.title = F"Backup Info - {backup['guild_name']} / [{backup['name']}]"
+                    chpag = commands.Paginator(suffix="...\n```", max_size=2048)
+                    for channel in backup['channels']:
+                        if type(channel) == discord.CategoryChannel: known = "/"
+                        if type(channel) == discord.TextChannel: known = "#"
+                        if type(channel) == discord.VoiceClient: known = ">"
+                        if type(channel) == discord.StageChannel: known = ")"
+                        chpag.add_line(F"{known} {channel.name}")
+                    ropag = commands.Paginator(suffix="...\n```", max_size=2048)
+                    for role in backup['roles']:
+                        ropag.add_line(role.name)
+                    if backup['icon']: bumbed.set_image(url=backup['icon'])
+                    if backup['banner']: bumbed.set_thumbnail(url=backup['banner'])
+                else:
+                    bumbed = "Couldn't find any backup with this name"
+            else:
+                bumbed = "You must pass an name"
+        elif option == "list":
+            bumbed.title = "Backups"
+            bumbed.description = "```"
+            for backup in await self.bot.mongodb.backups.find({"user_id": ctx.author.id}).to_list(length=100):
+                bumbed.description += F"{backup['guild_name']} / [{backup['name']}] - {discord.utils.format_dt(backup['time'], style='f')} ({discord.utils.format_dt(backup['time'], style='R')})\n"
+            bumbed.description += "```"
+        elif option == "load":
+            if value:
+                backup = await self.bot.mongodb.backups.find_one({"name": value, "user_id": ctx.author.id})
+                if backup:
+                    bumbed.title = F"Backup Info - {backup['guild_name']} / [{backup['name']}]"
+                    chpag = commands.Paginator(suffix="...\n```", max_size=2048)
+                    for channel in backup['channels']:
+                        if type(channel) == discord.CategoryChannel: known = "/"
+                        if type(channel) == discord.TextChannel: known = "#"
+                        if type(channel) == discord.VoiceClient: known = ">"
+                        if type(channel) == discord.StageChannel: known = ")"
+                        chpag.add_line(F"{known} {channel.name}")
+                    ropag = commands.Paginator(suffix="...\n```", max_size=2048)
+                    for role in backup['roles']:
+                        ropag.add_line(role.name)
+                    if backup['icon']: bumbed.set_image(url=backup['icon'])
+                    if backup['banner']: bumbed.set_thumbnail(url=backup['banner'])
+                    view = confirm.ViewConfirm(ctx)
+                    view.message = await ctx.reply(content="Are you sure if you want to load this backup?", embed=bumbed, view=view)
+                    await view.wait()
+                    if view.value:
+                        bumbed.title = F"Loading Backup!"
+                        bumbed.description = F"{backup['guild_name']} / [{backup['name']}]\nThis might take a while..."
+                        for channel in ctx.guild.channels:
+                            await channel.delete()
+                        for role in ctx.guild.roles:
+                            await role.delete()
+                        for emoji in ctx.guild.emojis:
+                            await emoji.delete()
+                        for sticker in ctx.guild.stickers:
+                            await sticker.delete()
+                        await asyncio.sleep(2.5)
+                        for channel in backup['channels']:
+                            await ctx.guild._create_channel(channel.name, type=channel.type, overwrites=channel.overwrites, category=channel.category)
+                        for role in backup['roles']:
+                            await ctx.guild.create_role(name=role.name, permissions=role.permissions, colour=role.colour, hoist=role.hoist, mentionable=role.mentionable)
+                        for emoji in backup['emojis']:
+                            await ctx.guild.create_custom_emoji(name=emoji.name, image=emoji.url)
+                        for sticker in backup['stickers']:
+                            await ctx.guild.create_sticker(name=sticker.name, image=sticker.url)
+                        await asyncio.sleep(2.5)
+                        bumbed.title = F"Backup Loaded!"
+                        return await ctx.guild.text_channels[0].send(embed=bumbed)
+                else:
+                    bumbed = "Couldn't find any backup with this name"
+            else:
+                bumbed = "You must pass an name"
+        await ctx.reply(embed=bumbed)
 
     # Notes
     @commands.command(name="notes", aliases=["nt"], help="Taking notes with this")
